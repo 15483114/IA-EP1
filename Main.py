9450a86c6655f52abc1812ad092400c87111e6e3
Gabriel Hoffman Silva <gabrielhoffman@usp.br>
2020-05-06 19:34:54 -0300

    asdsad

--- Main.py
+++ Main.py
@@ -1,3 +1,6 @@
+import numpy
+
+
 def size(file_path):
     with open(file_path, 'r') as file:
         size = 0
@@ -16,66 +19,88 @@ def createWeights(file_path):
     print(len(array))
     import random
     weights = []
-    for i in range(len(array)):
+    for i in range(len(array) - 1):
         weights.append(random.uniform(-0.5, 0.5))
     print(weights)
 
 
-def train(array, file_path):
+def train(file_path):
     import random
-    weights = []
-    for i in range(len(array)):
-        weights.append(random.uniform(-0.5, 0.5))
+    with open(file_path, 'r') as file:
+        line = file.readline()
+        array = line.split(',')
+    weights = numpy.zeros((10, len(array)))
+    for i in range(9):
+        for j in range(len(array) - 1):
+            weights[i][j] = (random.uniform(-0.5, 0.5))
 
-    n_epoch = 50
-    l_rate = 0.1
+    n_epoch = 10
+    learn_rate = 0.1
 
     for epoch in range(n_epoch):
-        sum_error = 0.0
+        sum_error = numpy.zeros(10)
         with open(file_path, 'r') as file:
-            for k in range(1000):
+            for k in range(10):
                 line = file.readline()
 
                 if len(line) > 100:
                     line = line.split(',')
-                    perceptron0 = weights[0]
-                    perceptron1 = weights[0]
-                    perceptron2 = weights[0]
-                    perceptron3 = weights[0]
-                    perceptron4 = weights[0]
-                    perceptron5 = weights[0]
-                    perceptron6 = weights[0]
-                    perceptron7 = weights[0]
-                    perceptron8 = weights[0]
-                    perceptron9 = weights[0]
+                    perceptron = numpy.zeros(10)
 
+                    for i in range(10):
+                        perceptron[i] = weights[i][0]
+
+                    for g in range(10):
+                        for i in range(1, len(line)):
+                            perceptron[g] += float(line[i]) / 255 * weights[g][i]
+
+                    error = numpy.zeros(10)
+
+                    for i in range(10):
+                        if line[0] == i:
+                            answer = 1
+                        else:
+                            answer = 0
+
+                        error[i] = answer - 1 / (1 + numpy.exp(-perceptron[i]))
+                        sum_error[i] += error[i]
+                        weights[i][0] = weights[i][0] + learn_rate * error[i]
+
+                        for j in range(1, len(line)):
+                            weights[i][j] = weights[i][0] + learn_rate * error[i] * float(line[j]) / 255
+
+        print(sum_error[0])
+
+    return weights
+
+
+def test(file_path, weights):
+    with open(file_path, 'r') as file:
+        for k in range(10):
+            line = file.readline()
+            if len(line) > 100:
+                line = line.split(',')
+                perceptron = numpy.zeros(10)
+
+                for i in range(10):
+                    perceptron[i] = weights[i][0]
+
+                for g in range(10):
                     for i in range(1, len(line)):
-                        perceptron0 += float(line[i]) / 255 * weights[i]
-                        perceptron1 += float(line[i]) / 255 * weights[i]
-                        perceptron2 += float(line[i]) / 255 * weights[i]
-                        perceptron3 += float(line[i]) / 255 * weights[i]
-                        perceptron4 += float(line[i]) / 255 * weights[i]
-                        perceptron5 += float(line[i]) / 255 * weights[i]
-                        perceptron6 += float(line[i]) / 255 * weights[i]
-                        perceptron7 += float(line[i]) / 255 * weights[i]
-                        perceptron8 += float(line[i]) / 255 * weights[i]
-                        perceptron9 += float(line[i]) / 255 * weights[i]
+                        perceptron[g] += float(line[i]) / 255 * weights[g][i]
 
-                    if (activation > 0):
-                        prediction = 1
-                    else:
-                        prediction = 0
+                result = 0
+                for i in range(len(perceptron)):
+                    if perceptron[result] < perceptron[i]:
+                        result = i
 
-                    if ((float(line[0]) == 7 and prediction == 1) or (float(line[0]) != 7) and prediction == 0):
-                        error = 0
-                    else:
-                        error = 1
+                print('correta: %d, resultado: %d'%(float(line[0]), result))
 
-                    sum_error += error ** 2
-                    weights[0] = weights[0] + l_rate * error
+file = open("weights.txt", "x")
+weights = train('mnist_treinamento.csv')
+numpy.savetxt(file, )
+file.close()
 
-                    for i in range(1, len(line)):
-                        weights[i] = weights[0] + l_rate * error * float(line[i]) / 255
-
-        # print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
-        print(weights)
+# file.open('weights.txt', 'r')
+# weights = file.readline()
+# test('mnist_teste.csv', weights)

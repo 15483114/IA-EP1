af67b9769f7bd78e7586798b85c645185666bde8
jessmelo <50722304+jessmelo@users.noreply.github.com>
2020-05-06 18:33:28 -0300

    Update converterCsv.py

--- conjunto MNIST/converterCsv.py
+++ conjunto MNIST/converterCsv.py
@@ -1,8 +1,8 @@
 import numpy
 
 from numpy import genfromtxt
-arrayCsv = genfromtxt('~/pré-processamento/mnist_teste.csv', delimiter=',')
+arrayCsv = genfromtxt('pré-processamento/mnist_teste.csv', delimiter=',')
 
 from PIL import Image
 im = Image.fromarray(arrayCsv)
-im.save("~/pré-processamento/imagens/your_file.png")
+im.save('pré-processamento/imagens/your_file.jpg')

3f6239f16cd468ea59c65dbb60ab3cb70d6cec50
jessmelo <50722304+jessmelo@users.noreply.github.com>
2020-05-06 18:20:08 -0300

    script de conversao

---
+++ conjunto MNIST/converterCsv.py
@@ -0,0 +1,8 @@
+import numpy
+
+from numpy import genfromtxt
+arrayCsv = genfromtxt('~/pré-processamento/mnist_teste.csv', delimiter=',')
+
+from PIL import Image
+im = Image.fromarray(arrayCsv)
+im.save("~/pré-processamento/imagens/your_file.png")

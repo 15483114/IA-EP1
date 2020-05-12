4402c89bbd437dd4533a8d3a2fb746b13faf6385
15483114 <50025985+15483114@users.noreply.github.com>
2020-05-12 19:40:55 -0300

    Tentei deixar mais claro
    Ainda não está funcionando, mas tentei deixar mais parecido com o vocabulário dos slides, btw, não vai funcionar no pc de vcs pq estou chamando o csv de 'mnist_teste.csv', então não estou usando o que está na pasta, teria que colocar esse main junto com o csv numa mesma pasta, pra poupar tempo, não pesquisei como usar o rar que tá na pasta, depois eu vejo isso.

--- Main.py
+++ Main.py
@@ -1,81 +1,63 @@
-def size(file_path):
-    with open(file_path, 'r') as file:
-        size = 0
-        while (1):
-            if (file.readline() != ''):
-                size += 1
-            else:
-                break;
-        print('number of lines: %d' % size)
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 12 14:24:20 2020
 
+@author: kuo incrível
+"""
 
-def createWeights(file_path):
-    with open(file_path, 'r') as file:
-        line = file.readline()
-        array = line.split(',')
-    print(len(array))
-    import random
+import csv
+f = open('mnist_teste.csv')
+treino = csv.reader(f)
+
+# separando as linhas
+xs=[]
+for row in treino:
+    xs.append(row)
+
+  
+import random
+
+def generate_weights():
     weights = []
-    for i in range(len(array)):
-        weights.append(random.uniform(-0.5, 0.5))
-    print(weights)
 
+    for i in range(0, 785):
+        x = random.uniform(-0.5, 0.5)
+        weights.append(x)
+        
+    return weights
 
-def train(array, file_path):
-    import random
-    weights = []
-    for i in range(len(array)):
-        weights.append(random.uniform(-0.5, 0.5))
+def oE(x,w,i): # calcula o resultado obtido
+    Soma=0 
+    for i in range (1,785,1): #lenght? len(treino.iloc[1])    
+        Soma=Soma+ x[i]*w[i]
+        if Soma==x[i][0]:
+            return 1
+        else:
+            return 0
 
-    n_epoch = 50
-    l_rate = 0.1
+def tE(perceptronNumeroX, NumeroDoTreino): # fornece o resultado esperado
+    if (perceptronNumeroX == NumeroDoTreino):
+        return 1
+    else:
+        return 0
 
-    for epoch in range(n_epoch):
-        sum_error = 0.0
-        with open(file_path, 'r') as file:
-            for k in range(1000):
-                line = file.readline()
+n=0.1
+epoca=5
 
-                if len(line) > 100:
-                    line = line.split(',')
-                    perceptron0 = weights[0]
-                    perceptron1 = weights[0]
-                    perceptron2 = weights[0]
-                    perceptron3 = weights[0]
-                    perceptron4 = weights[0]
-                    perceptron5 = weights[0]
-                    perceptron6 = weights[0]
-                    perceptron7 = weights[0]
-                    perceptron8 = weights[0]
-                    perceptron9 = weights[0]
+# iniciando a regra do perceptron
+weights= []
+for o in range(10):
+    w= generate_weights()
+    weights.append(w)
 
-                    for i in range(1, len(line)):
-                        perceptron0 += float(line[i]) / 255 * weights[i]
-                        perceptron1 += float(line[i]) / 255 * weights[i]
-                        perceptron2 += float(line[i]) / 255 * weights[i]
-                        perceptron3 += float(line[i]) / 255 * weights[i]
-                        perceptron4 += float(line[i]) / 255 * weights[i]
-                        perceptron5 += float(line[i]) / 255 * weights[i]
-                        perceptron6 += float(line[i]) / 255 * weights[i]
-                        perceptron7 += float(line[i]) / 255 * weights[i]
-                        perceptron8 += float(line[i]) / 255 * weights[i]
-                        perceptron9 += float(line[i]) / 255 * weights[i]
-
-                    if (activation > 0):
-                        prediction = 1
-                    else:
-                        prediction = 0
-
-                    if ((float(line[0]) == 7 and prediction == 1) or (float(line[0]) != 7) and prediction == 0):
-                        error = 0
-                    else:
-                        error = 1
-
-                    sum_error += error ** 2
-                    weights[0] = weights[0] + l_rate * error
-
-                    for i in range(1, len(line)):
-                        weights[i] = weights[0] + l_rate * error * float(line[i]) / 255
-
-        # print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
-        print(weights)
+for k in range (epoca):
+    for i in range (99): #trocar por size of rows
+        bias=[[], [] ,[] ,[] ,[] ,[] ,[], [], [], [] ] #bias/delta que ta no slide
+        for j in range (1,785):
+            for h in range(10):
+                
+                oE=oE(xs[i][j],weights[h],h)
+                tE= tE(h,xs[i][j])
+                bias[h][j]= n*(tE-oE)*xs[i][j]
+                weights[h][j] = weights[h][j]+ bias
+                print(bias[h])

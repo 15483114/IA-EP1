f993273c2f4344df7cc2f0d2145054929532698c
15483114 <50025985+15483114@users.noreply.github.com>
2020-05-12 19:59:58 -0300

    pequeninas correções nos índices

--- Main.py
+++ Main.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May 12 14:24:20 2020
 
-@author: kuo incrível
-"""
 
 import csv
 f = open('mnist_teste.csv')
@@ -26,23 +21,23 @@ def generate_weights():
         
     return weights
 
-def oE(x,w,i): # calcula o resultado obtido
+def oe(x,w): # calcula o resultado obtido
     Soma=0 
-    for i in range (1,785,1): #lenght? len(treino.iloc[1])    
+    for i in range (1,783,1): #lenght? len(treino.iloc[1])    
         Soma=Soma+ x[i]*w[i]
         if Soma==x[i][0]:
             return 1
         else:
             return 0
 
-def tE(perceptronNumeroX, NumeroDoTreino): # fornece o resultado esperado
+def te(perceptronNumeroX, NumeroDoTreino): # fornece o resultado esperado
     if (perceptronNumeroX == NumeroDoTreino):
         return 1
     else:
         return 0
 
 n=0.1
-epoca=5
+epoca=1
 
 # iniciando a regra do perceptron
 weights= []
@@ -50,14 +45,18 @@ for o in range(10):
     w= generate_weights()
     weights.append(w)
 
+
 for k in range (epoca):
-    for i in range (99): #trocar por size of rows
+    for i in range (9999): #trocar por size of rows
         bias=[[], [] ,[] ,[] ,[] ,[] ,[], [], [], [] ] #bias/delta que ta no slide
         for j in range (1,785):
             for h in range(10):
                 
-                oE=oE(xs[i][j],weights[h],h)
-                tE= tE(h,xs[i][j])
+                oE=oe(xs[j],weights[h])
+                print(oE)
+                tE= te(h,xs[i][0])
                 bias[h][j]= n*(tE-oE)*xs[i][j]
                 weights[h][j] = weights[h][j]+ bias
                 print(bias[h])
+                
+    

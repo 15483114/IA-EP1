b42e6a40d6f4940cb4b4fac72a77c0337a1210bf
15483114 <50025985+15483114@users.noreply.github.com>
2020-05-12 20:06:44 -0300

    tá melhor mas ainda não tá bom
    Eu acho que tem alguma coisa haver com o k, i, j,h sla, mas tô mt confusa pra resolver isso agora

--- Main.py
+++ Main.py
@@ -1,4 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 12 14:24:20 2020
 
+@author: kuo incrível
+"""
 
 import csv
 f = open('mnist_teste.csv')
@@ -45,18 +50,18 @@ for o in range(10):
     w= generate_weights()
     weights.append(w)
 
+bias=[[], [] ,[] ,[] ,[] ,[] ,[], [], [], [] ] #bias/delta que ta no slide
 
 for k in range (epoca):
-    for i in range (9999): #trocar por size of rows
-        bias=[[], [] ,[] ,[] ,[] ,[] ,[], [], [], [] ] #bias/delta que ta no slide
+    for i in range (9999): #trocar por alguma forma genérica que indique quantidade de linhas
+        
         for j in range (1,785):
             for h in range(10):
                 
                 oE=oe(xs[j],weights[h])
-                print(oE)
                 tE= te(h,xs[i][0])
                 bias[h][j]= n*(tE-oE)*xs[i][j]
-                weights[h][j] = weights[h][j]+ bias
-                print(bias[h])
+                weights[h][j] = weights[h][j]+ bias[h][j]
+                
                 
     

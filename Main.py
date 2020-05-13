d66d8557a2580343bd555f93129e735dcc42067b
Gabriel Hoffman Silva <gabrielhoffman@usp.br>
2020-05-13 19:29:10 -0300

    ada

--- Main.py
+++ Main.py
@@ -1,114 +1,117 @@
+import random
+
 import numpy
 
-
-def train(file_path):
-    weights = create_weights(file_path)
-    n_epoch = 100
-    learn_rate = 0.01
-
-    for epoch in range(n_epoch):
-        acertos = 0
-        with open(file_path, 'r') as file:
-            for k in range(100):
-                row = file.readline()
-
-                if len(row) > 1:
-                    row = row.split(',')
-                    activation = numpy.zeros(10)
-                    error = numpy.zeros(10)
-                    row = pre_process(row)
-
-                    for i in range(10):
-                        calculate_activation(i, row, activation, weights)
-                        answer = calculate_error(error, i, row, activation)
-                        update_weights(error, i, learn_rate, row, weights)
-                        # if i == 0:
-                        #     print(
-                        #         'activation:%f answer:%f error:%f' % (activation[i], answer, error[i]))
-
-                    result = 0
-                    for i in range(10):
-                        if (activation[result] > activation[i]):
-                            result = i
-                    # print(result)
-                    if result == float(row[0]):
-                        acertos += 1
-        print('epoch:%d acertos:%d' % (epoch, acertos))
-
-        # print(weights[0][300])
-
-    return weights
+size = 0
+learn_rate = 0.1
+epocas = 50
 
 
-def update_weights(error, i, learn_rate, row, weights):
-    weights[i][0] = weights[i][0] + learn_rate * error[i]
-    for j in range(1, len(row)):
-        weights[i][j] = weights[i][j] + learn_rate * error[i] * float(row[i])
+def recebe_linha(line):
+    line = line.split(',')
+    line[784] = 0
+    return line
 
 
-def calculate_activation(i, line, activation, weights):
-    activation[i] = weights[i][0]
-    for g in range(1, len(line)):
-        activation[i] += line[g] * weights[i][g]
-
-
-def calculate_error(error, i, line, perceptron):
-    if int(line[0]) == i:
-        answer = 1
-    else:
-        answer = 0
-    # perceptron[i] = 1 / (1 + numpy.exp(-perceptron[i]))
-    # error[i] = answer - perceptron[i]
-    # if answer == 0:
-        # error[i] /= 2
-    if answer == 1:
-        error[i] = answer - perceptron[i]
-        error[i] *= 100
-    return answer
-
-
-def create_weights(file_path):
-    import random
-    with open(file_path, 'r') as file:
-        line = file.readline()
-        array = line.split(',')
-    weights = numpy.zeros((10, len(array)))
-    for i in range(9):
-        for j in range(len(array)):
-            weights[i][j] = (random.uniform(-0.5, 0.5))
-    return weights
-
-
-def pre_process(line):
-    for i in range(1, len(line)):
+def pre_processamento(line):
+    for i in range(len(line) - 1):
         line[i] = float(line[i]) / 255
     return line
 
 
-def test(file_path, weights):
-    with open(file_path, 'r') as file:
-        for k in range(100):
-            row = file.readline()
-
-            if len(row) > 1:
-                row = row.split(',')
-                perceptron = numpy.zeros(10)
-                error = numpy.zeros(10)
-                row = pre_process(row)
-
-                for i in range(10):
-                    calculate_activation(i, row, perceptron, weights)
-
-                result = 0
-                for i in range(10):
-                    if (perceptron[result] > perceptron[i]):
-                        result = i
-
-                # print('%d -- %d' % (int(row[0]), result))
+def define_resposta(line):
+    if line[0] == 0:
+        answer = 1
+    else:
+        answer = 0
+    return answer
 
 
-weights = train('mnist_treinamento.csv')
+def inicializa_pesos_aleatorios(weights):
+    for i in range(size):
+        weights[i] = random.uniform(-0.5, 0.5)
+    return weights
 
-test('mnist_teste.csv', weights)
 
+def calcula_ativacao(weights, line):
+    # print(line)
+    ativacao = weights[0]
+    for i in range(size):
+        ativacao += weights[i] * line[i]
+    ativacao = 1 / (1 + numpy.exp(-ativacao))
+    return ativacao
+
+
+def calcula_erro(ativacao, answer):
+    # erro = numpy.power((answer - ativacao), 2)
+    erro = answer - ativacao
+    return erro
+
+
+def atualiza_pesos(erro, weights, line):
+    weights[0] += learn_rate * erro
+    for i in range(size):
+        weights[i] += learn_rate * erro * line[i] + 0.0001
+    return weights
+
+
+def create_weights():
+    global size
+    with open('mnist_treinamento.csv') as file:
+        line = file.readline()
+        line = recebe_linha(line)
+        size = len(line)
+        weights = numpy.zeros(size)
+        weights = inicializa_pesos_aleatorios(weights)
+        # print(weights)
+        return weights
+
+
+def train():
+    x = 60000
+    print('TREINANDO')
+    weights = create_weights()
+    for i in range(epocas):
+        percentual_acerto = 0
+        with open('mnist_treinamento.csv') as file:
+            line = file.readline()
+            for j in range(x):
+                line = recebe_linha(line)
+                line = pre_processamento(line)
+                answer = define_resposta(line)
+                ativacao = calcula_ativacao(weights, line)
+                erro = calcula_erro(ativacao, answer)
+                weights = atualiza_pesos(erro, weights, line)
+                percentual_acerto += erro
+                # print('%d %f' % (answer, ativacao))
+                line = file.readline()
+                # learn_rate = 1 / (1 + numpy.exp(-erro))
+        # print(weights)
+        print('erro:%f' % (percentual_acerto))
+        if abs(percentual_acerto) < 234:
+            break
+    return weights
+
+
+def test(weights):
+    print('TESTANDO')
+    percentual_acerto = 0
+    with open('mnist_teste.csv') as file:
+        line = file.readline()
+        for j in range(10000):
+            line = recebe_linha(line)
+
+            line = pre_processamento(line)
+
+            answer = define_resposta(line)
+
+            ativacao = calcula_ativacao(weights, line)
+
+            erro = calcula_erro(ativacao, answer)
+
+            print('%d %f' % (answer, ativacao))
+            line = file.readline()
+
+weights = train()
+test(weights)
 exit()
